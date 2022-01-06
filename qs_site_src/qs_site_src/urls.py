710d4dc5680f42abc91bc4be119c224d50352480
joedomabylv <joedomabylv@gmail.com>
2022-01-06 16:36:51 -0700

    changed file name again

--- qs_site_src/qs_site_src/urls.py
+++ qs_site_src/qs_site_src/urls.py
@@ -1,4 +1,4 @@
-"""quicksched URL Configuration
+"""qs_site_src URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- qs_site_src/qs_site_src/wsgi.py
+++ qs_site_src/qs_site_src/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for quicksched project.
+WSGI config for qs_site_src project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quicksched.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qs_site_src.settings')
 
 application = get_wsgi_application()
