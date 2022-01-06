db02cf307cce953085c4ecd67d51c43bfcca24a0
joedomabylv <joedomabylv@gmail.com>
2022-01-06 16:18:04 -0700

    rename prototype file

--- src/quicksched/asgi.py
+++ src/quicksched/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for quicksched_prototype project.
+ASGI config for quicksched project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quicksched_prototype.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quicksched.settings')
 
 application = get_asgi_application()
--- src/quicksched/wsgi.py
+++ src/quicksched/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for quicksched_prototype project.
+WSGI config for quicksched project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quicksched_prototype.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quicksched.settings')
 
 application = get_wsgi_application()
